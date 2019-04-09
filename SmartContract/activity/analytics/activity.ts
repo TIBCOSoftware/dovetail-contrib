791b89de995fc6b73cfc92493a546b53b1d693e0
torresashjian <migueltorreslopez@gmail.com>
2019-04-09 12:18:24 -0700

    Add input object

--- SmartContract/activity/analytics/activity.ts
+++ SmartContract/activity/analytics/activity.ts
@@ -27,54 +27,5 @@ export class AnalyticsActivityContributionHandler extends WiServiceHandlerContri
   constructor(private injector: Injector, private http: Http) {
     super(injector, http);
 }
-       
-    value = (fieldName: string, context: IActivityContribution): any | Observable<any> => {
-        let action = context.getField("operation").value;
-        let asset = context.getField("assetName").value;
-        let isArray = false;
-        
-        let conId = context.getField("model").value;
-    
-        switch(fieldName){
-            case "input":
-                if(Boolean(conId) == false || Boolean(asset) == false)
-                    return null;
-
-                return Observable.create(observer => {
-                    this.getSchemas(conId).subscribe( schemas => {
-                        observer.next(schemas[asset]);
-                        
-                    });
-                });
-            case "output":
-                if(Boolean(conId) == false || Boolean(asset) == false)
-                    return null;
-
-                return Observable.create(observer => {
-                    this.getSchemas(conId).subscribe( schemas => {
-                        observer.next(schemas[asset]);
-                    });
-                });
-            default:
-                return null;
-        }
-    }
-    getSchemas(conId):  Observable<any> {
-        let schemas = new Map();
-        return Observable.create(observer => {
-            WiContributionUtils.getConnection(this.http, conId)
-                            .map(data => data)
-                            .subscribe(data => {
-                                let schemas = new Map();
-                                for (let setting of data.settings) {
-                                    if(setting.name === "schemas") {
-                                        setting.value.map(item => schemas[item[0]] = item[1]);
-                                        observer.next(schemas);
-                                        break;
-                                    }
-                                }
-                            });
-                        });
-    }
 
 }
