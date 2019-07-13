3cfd5790bc4210e91fa0c377a35def7f8657b72b
Yueming Xu <yxu@tibco.com>
2019-07-13 14:55:19 -0600

    tool to generate contract metadata from flogo model

--- hyperledger-fabric/fabric-tools/cmd/metadata-impl.go
+++ hyperledger-fabric/fabric-tools/cmd/metadata-impl.go
@@ -13,11 +13,14 @@ import (
 	"github.com/pkg/errors"
 )
 
-// TransactionSchemas describes data schemas in metadata of a transaction
-type TransactionSchemas struct {
-	Parameters string
-	Transient  string
-	Returns    string
+// Transaction describes data schemas in metadata of a transaction
+type Transaction struct {
+	Name        string          `json:"name,omitempty"`
+	Description string          `json:"description,omitempty"`
+	Operation   string          `json:"operation,omitempty"`
+	Parameters  json.RawMessage `json:"parameters,omitempty"`
+	Transient   json.RawMessage `json:"transient,omitempty"`
+	Returns     json.RawMessage `json:"returns,omitempty"`
 }
 
 // SchemaValue describes json schema of a parameter value
@@ -28,14 +31,37 @@ type SchemaValue struct {
 	Items      json.RawMessage `json:"items,omitempty"`
 }
 
-var schemaCache map[string]SchemaValue
+// Contract describes json schema of a contract
+type Contract struct {
+	Name         string         `json:"name"`
+	Transactions []*Transaction `json:"transactions"`
+}
+
+// Info describes metadata info
+type Info struct {
+	Title   string `json:"title"`
+	Version string `json:"version"`
+}
+
+// Metadata describes smart contracts of an app
+type Metadata struct {
+	Schema     string               `json:"$schema"`
+	Info       Info                 `json:"info"`
+	Contracts  map[string]*Contract `json:"contracts"`
+	Components struct {
+		Schemas map[string]SchemaValue `json:"schemas,omitempty"`
+	} `json:"components,omitempty"`
+}
+
+var schemaCache map[string]*SchemaValue
 var sharedSchema map[string]string
 
 func init() {
-	schemaCache = make(map[string]SchemaValue)
+	schemaCache = make(map[string]*SchemaValue)
 	sharedSchema = make(map[string]string)
 }
 
+// generate contract metadata from flogo model
 func generateMetadata(appfile, outpath string) error {
 	appconfig, err := ioutil.ReadFile(appfile)
 	if err != nil {
@@ -61,25 +87,76 @@ func generateMetadata(appfile, outpath string) error {
 		return errors.Wrapf(err, "failed to extract data from flogo config file %s", appfile)
 	}
 
+	// construct output metadata
+	metadata := Metadata{
+		Schema:    "http://json-schema.org/draft-04/schema#",
+		Info:      Info{Title: contractData.Name, Version: contractData.Version},
+		Contracts: map[string]*Contract{},
+	}
+	contract := Contract{Name: contractData.Name}
+	metadata.Contracts[contract.Name] = &contract
+
 	// select all handlers of the first fabric transaction trigger
 	for _, trig := range contractData.Triggers {
 		if trig.Ref == "#transaction" {
 			for _, handler := range trig.Handlers {
-				fmt.Printf("name %s\n", handler.Settings.Name)
-				txnSchemas := extractTransactionSchema(handler.Schemas)
-				fmt.Printf("schemas: %+v\n", txnSchemas)
+				fmt.Printf("transaction name %s\n", handler.Settings.Name)
+				txn := extractTransactionSchema(handler.Schemas)
+				txn.Name = handler.Settings.Name
+				txn.Description = handler.Description
+				// TODO: determine if it is invoke or query
+				txn.Operation = "invoke"
+				contract.Transactions = append(contract.Transactions, &txn)
 			}
 			break
 		}
 	}
-	for k, v := range sharedSchema {
-		fmt.Printf("shared schema type %s: %s\n", v, k)
+
+	// set unique id for shared schema
+	setSharedSchemaRef(contractData.Name)
+	if len(sharedSchema) > 0 {
+		metadata.Components.Schemas = map[string]SchemaValue{}
+		for k := range sharedSchema {
+			sv := schemaCache[k]
+			metadata.Components.Schemas[sv.Ref] = *sv
+		}
 	}
-	return nil
+
+	// use shared schema in transacions
+	for _, txn := range contract.Transactions {
+		replaceSharedSchema(txn)
+	}
+	metabytes, _ := json.MarshalIndent(metadata, "", "    ")
+	fmt.Println("write metadata to", outpath)
+	return writeFile(outpath, metabytes)
 }
 
-func extractTransactionSchema(schemas map[string]json.RawMessage) TransactionSchemas {
-	var parameters, transient, returns string
+func replaceSharedSchema(txn *Transaction) {
+	if txn.Parameters != nil {
+		txn.Parameters = toSharedSchema(txn.Parameters)
+	}
+	if txn.Transient != nil {
+		txn.Transient = toSharedSchema(txn.Transient)
+	}
+	if txn.Returns != nil {
+		txn.Returns = toSharedSchema(txn.Returns)
+	}
+}
+
+func toSharedSchema(schema []byte) []byte {
+	svkey := string(schema)
+	if _, ok := sharedSchema[svkey]; ok {
+		sv := schemaCache[svkey]
+		shared := fmt.Sprintf(`{
+			"$ref": "#/components/schemas/%s"
+		}`, sv.Ref)
+		return []byte(shared)
+	}
+	return schema
+}
+
+func extractTransactionSchema(schemas map[string]json.RawMessage) Transaction {
+	var parameters, transient, returns []byte
 	if reply, ok := schemas["reply"]; ok {
 		var r struct {
 			Returns struct {
@@ -114,10 +191,10 @@ func extractTransactionSchema(schemas map[string]json.RawMessage) TransactionSch
 			}
 		}
 	}
-	return TransactionSchemas{Parameters: parameters, Transient: transient, Returns: returns}
+	return Transaction{Parameters: parameters, Transient: transient, Returns: returns}
 }
 
-func addSchemaToCache(value string) string {
+func addSchemaToCache(value string) []byte {
 	var sv SchemaValue
 	if err := json.Unmarshal([]byte(value), &sv); err != nil {
 		fmt.Printf("failed to unmarshal transient value: %+v\n", err)
@@ -130,10 +207,40 @@ func addSchemaToCache(value string) string {
 			if _, ok := schemaCache[skey]; ok {
 				sharedSchema[skey] = sv.Type
 			} else {
-				schemaCache[skey] = sv
+				schemaCache[skey] = &sv
 			}
-			return skey
+			return svbytes
 		}
 	}
-	return ""
+	return nil
+}
+
+func setSharedSchemaRef(appname string) {
+	arraySeq := 0
+	objectSeq := 0
+	for k := range sharedSchema {
+		sv := schemaCache[k]
+		if sv.Type == "array" {
+			sv.Ref = fmt.Sprintf("%s_array%d", appname, arraySeq)
+			arraySeq++
+		} else {
+			if n, err := objectPropertyCount(sv.Properties); err == nil && n > 2 {
+				sv.Ref = fmt.Sprintf("%s_%d", appname, objectSeq)
+				objectSeq++
+			} else {
+				delete(sharedSchema, k)
+				if err != nil {
+					fmt.Printf("remove shared schema %s due to error: %+v\n", k, err)
+				}
+			}
+		}
+	}
+}
+
+func objectPropertyCount(schema []byte) (int, error) {
+	var props map[string]interface{}
+	if err := json.Unmarshal(schema, &props); err != nil {
+		return -1, errors.Wrapf(err, "failed to unmarshal object properties: %s", string(schema))
+	}
+	return len(props), nil
 }
