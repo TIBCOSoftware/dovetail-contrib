6d89867e641b91a5f1236688cd11bb9ba210a709
torresashjian <migueltorreslopez@gmail.com>
2019-08-05 19:35:47 -0700

    Add release notes

---
+++ scripts/release_notes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+release_notes=$(<release_notes.txt)
+echo "${release_notes}"
--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 version=$(<version.txt)
+if [ ! $TRAVIS_BRANCH == "master" ]
+then
+    version=${version}+$TRAVIS_BRANCH
+fi
 echo "${version}"
