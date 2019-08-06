091f0dbf9fa37b39aa83d987898d29e6d4c72803
torresashjian <migueltorreslopez@gmail.com>
2019-08-05 20:00:27 -0700

    Add release notes

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,6 +2,6 @@
 version=$(<version.txt)
 if [ ! $TRAVIS_BRANCH == "master" ]
 then
-    version=${version}_$TRAVIS_BRANCH
+    version=${version}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}
 fi
 echo "${version}"
