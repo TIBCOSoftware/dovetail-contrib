afbc18e29ea2a8d64d7b83eba0be57e39ffe1484
torresashjian <migueltorreslopez@gmail.com>
2019-08-06 13:24:57 -0700

    Add files

--- scripts/hyperledger-fabric-artifacts.sh
+++ scripts/hyperledger-fabric-artifacts.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo "Building fabric extension"
 build_tag=$1
 if [ -d .target/hyperledger-fabric ]; then
   rm -rf .target/hyperledger-fabric
 fi
 mkdir -p .target/hyperledger-fabric
-zip -r .target/hyperledger-fabric/fabric-extension-${build_tag////-}.zip hyperledger-fabric/fabric
+
+cd hyperledger-fabric
+
+echo "Building fabric extension"
+zip -r ../.target/hyperledger-fabric/fabric-extension-${build_tag////-}.zip fabric
 
 echo "Building fabric client extension"
-zip -r .target/hyperledger-fabric/fabric-client-extension-${build_tag////-}.zip hyperledger-fabric/fabclient
+zip -r ../.target/hyperledger-fabric/fabric-client-extension-${build_tag////-}.zip fabclient
 
 echo "Building fabric functions"
-zip -r .target/hyperledger-fabric/fabric-function-${build_tag////-}.zip hyperledger-fabric/function
+zip -r ../.target/hyperledger-fabric/fabric-function-${build_tag////-}.zip function
