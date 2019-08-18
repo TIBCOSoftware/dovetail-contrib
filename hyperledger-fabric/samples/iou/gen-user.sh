6ff36a1cd8df78655e4af915d2083ad6d745e585
Yueming Xu <yxu@tibco.com>
2019-08-18 00:46:55 -0600

    develop iou sample

--- hyperledger-fabric/samples/iou/gen-user.sh
+++ hyperledger-fabric/samples/iou/gen-user.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# generate user key and cert with attributes for ABAC test
+# generate user key and cert with attribute bank, admin, alias, and email
 # usage: gen-user.sh name org
 #  e.g., gen-user/sh Alice 1
+#  user-name ending with "Admin" acts as bank admin 
 
-USER=${1:-"User3"}
+USER=${1:-"Alice"}
 ORG=org1.example.com
 PORT=7054
+BANK=EURBank
 if [ "$2" == "2" ]; then
   ORG=org2.example.com
   PORT=8054
+  BANK=USDBank
+fi
+
+ADMIN="false"
+if [[ "${USER}" == *Admin ]]; then
+  echo "${USER} is bank admin"
+  ADMIN="true"
 fi
 
 FABRIC_SAMPLE_PATH=${GOPATH}/src/github.com/hyperledger/fabric-samples/first-network
@@ -41,13 +50,17 @@ openssl x509 -noout -text -in ${FABRIC_CA_CLIENT_HOME}/msp/cacerts/localhost-${P
 fabric-ca-client enroll -u http://admin:adminpw@localhost:${PORT}
 
 # register and enroll new user
-fabric-ca-client register --id.name ''"${USER}@${ORG}"'' --id.secret ${USER}pw --id.type client --id.attrs 'abac.init=true:ecert,email='"${USER}@${ORG}"''
+fabric-ca-client register --id.name ''"${USER}"'' --id.secret ${USER}pw --id.type client --id.attrs 'admin='"${ADMIN}"':ecert,alias='"${USER}"',bank='"${BANK}"',email='"${USER}@${ORG}"''
 export FABRIC_CA_CLIENT_HOME=${WORK}/${USER}\@${ORG}
-fabric-ca-client enroll -u http://${USER}@${ORG}:${USER}pw@localhost:${PORT} --enrollment.attrs "abac.init,email" -M ${FABRIC_CA_CLIENT_HOME}/msp
+fabric-ca-client enroll -u http://${USER}:${USER}pw@localhost:${PORT} --enrollment.attrs "admin,alias,bank,email" -M ${FABRIC_CA_CLIENT_HOME}/msp
 openssl x509 -noout -text -in ${WORK}/${USER}\@${ORG}/msp/signcerts/cert.pem
 
 # copy key and cert to first-network sample crypto-config
 cd ${FABRIC_SAMPLE_PATH}/crypto-config/peerOrganizations/${ORG}/users
+if [ -d "${USER}@${ORG}" ]; then
+  echo "remove old crypto ${USER}@${ORG}"
+  rm -Rf ${USER}\@${ORG}
+fi
 cp -R User1\@${ORG} ${USER}\@${ORG}
 cd ${USER}\@${ORG}
 rm -R msp/keystore
