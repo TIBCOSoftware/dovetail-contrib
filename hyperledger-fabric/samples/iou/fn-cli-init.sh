90f4de2b51dda1e59c7073bba82bf93ddb3dfdd1
Yueming Xu <yxu@tibco.com>
2019-08-18 20:26:17 -0600

    debug iou sample

--- hyperledger-fabric/samples/iou/fn-cli-init.sh
+++ hyperledger-fabric/samples/iou/fn-cli-init.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-# install and instantiate abac_cc in the Fabric sample first-network
+# install and instantiate iou_cc in the Fabric sample first-network
 # execute this script from the scripts folder of the cli docker container
 
 . ./utils.sh
+CCNAME=iou_cc
 
-echo "install abac_cc on peer0 org1"
-peer chaincode install -n abac_cc -v 1.0 -p github.com/chaincode/abac_cc
+echo "install ${CCNAME} on peer0 org1"
+peer chaincode install -n ${CCNAME} -v 1.0 -p github.com/chaincode/${CCNAME}
 
+echo "install ${CCNAME} on peer1 org1"
+setGlobals 1 1
+peer chaincode install -n ${CCNAME} -v 1.0 -p github.com/chaincode/${CCNAME}
+
+echo "install ${CCNAME} on peer0 org2"
 setGlobals 0 2
-echo "install abac_cc on peer0 org2"
-peer chaincode install -n abac_cc -v 1.0 -p github.com/chaincode/abac_cc
+peer chaincode install -n ${CCNAME} -v 1.0 -p github.com/chaincode/${CCNAME}
 
-echo "instantiate abac_cc"
+echo "install ${CCNAME} on peer1 org2"
+setGlobals 1 2
+peer chaincode install -n ${CCNAME} -v 1.0 -p github.com/chaincode/${CCNAME}
+
+echo "instantiate ${CCNAME}"
+# Note: some transactions (createAccount) can be done by only one of the orgs, and so the endorsement policy uses 'OR' for simplicity here;
+# real production should use `AND` for most transactions; createAccount may use a different endorsement policy by packaging it as a second chaincode.
 ORDERER_ARGS="-o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-peer chaincode instantiate $ORDERER_ARGS -C mychannel -n abac_cc -v 1.0 -c '{"Args":["init"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
+peer chaincode instantiate $ORDERER_ARGS -C mychannel -n ${CCNAME} -v 1.0 -c '{"Args":["init"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')" --collections-config /opt/gopath/src/github.com/chaincode/${CCNAME}/collections_config.json
