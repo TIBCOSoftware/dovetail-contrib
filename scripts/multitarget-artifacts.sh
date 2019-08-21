66de9c03cd236cb5d7315aebbebfa62bfa1d9a8e
torresashjian <migueltorreslopez@gmail.com>
2019-08-21 14:23:29 -0700

    adding general to multitarget

--- scripts/multitarget-artifacts.sh
+++ scripts/multitarget-artifacts.sh
@@ -8,5 +8,8 @@ mkdir -p .target/multitarget
 echo "Building multitarget extension"
 zip -r .target/multitarget/multitarget-extension-${build_tag////-}.zip multitarget/multitarget
 
+echo "Building multitarget general extension"
+zip -r .target/multitarget/multitarget-general-extension-${build_tag////-}.zip multitarget/general
+
 echo "Building multitarget functions"
 zip -r .target/multitarget/multitarget-function-${build_tag////-}.zip multitarget/function
